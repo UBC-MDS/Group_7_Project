9ffbce2de8c8be3d546acb4ac4a32e1faf7b14b6
chenhanx <chenhanx@student.ubc.ca>
2023-11-24 18:39:00 -0800

    add docstring

--- src/models/lr_model.py
+++ src/models/lr_model.py
@@ -2,6 +2,39 @@ from src.models.model import *
 from sklearn.linear_model import LogisticRegression
 
 class LRModel(Model):
+    """
+    Logistic Regression Model with hyperparameter tuning.
+
+    Parameters:
+    - C_range: list or array-like
+        Range of regularization parameter values for hyperparameter tuning.
+
+    Inherits from the Model class.
+
+    Attributes:
+    - Inherits attributes from the Model class.
+    
+    Methods:
+    - __init__(C_range):
+        Constructor method. Initializes the LRModel with a specified range of regularization parameter values.
+    
+    - create_pipeline():
+        Creates the machine learning pipeline for Logistic Regression with class weights.
+    
+    Example:
+    ```python
+    # Create an instance of the LRModel class with a specific range of C values
+    lr_model_instance = LRModel(C_range=[0.1, 1, 10])
+
+    # Set the preprocessor and create the pipeline
+    lr_model_instance.set_preprocessor(my_preprocessor)
+
+    # Perform hyperparameter tuning
+    best_score, best_params = lr_model_instance.search_cv(X_train, y_train)
+    ```
+
+    Note: This class assumes the use of Altair for plotting. Make sure to have the necessary dependencies installed.
+    """
     def __init__(self, C_range):
         super().__init__()
         self.create_pipeline()
