a071acd5e13b8951c7a09ee10d7485852a0d4437
gtmx23 <greteltan21@gmail.com>
2023-11-29 11:59:09 -0800

    Edited script for preprocessing

--- scripts/preprocessor_script.py
+++ scripts/preprocessor_script.py
@@ -8,15 +8,15 @@ from sklearn import set_config
 
 
 @click.command
-@click.option('--raw_data', type=str, help="Path for accessing raw data")
+@click.option('--raw_train_data', type=str, help="Path for accessing raw train data")
+@click.option('--raw_test_data', type=str, help="Path for accessing raw test data")
 # @click.option('--output_dropped_train', type=str, help="Path for writing file containing train data with dropped features")
 # @click.option('--output_dropped_test', type=str, help="Path for writing file containing test data with dropped features")
-# Consider deleting this and just storing preprocessing object
 @click.option('--output_preprocessed_data', type=str, help="Path for writing files containing preprocessed data")
 @click.option('--output_preprocessor', type=str, help="Path for writing the file containing the preprocessor object")
 @click.option('--seed', type=int, help="Random seed", default=522)
 
-def main(raw_data, output_preprocessed_data, output_preprocessor, seed):
+def main(raw_train_data, raw_test_data, output_preprocessed_data, output_preprocessor, seed):
     """
     This script drops columns which have too many null values to be useful for analysis as well as NA values,
     then splits the dataframe into the train and test splits and saves them to csv files, 
@@ -24,37 +24,38 @@ def main(raw_data, output_preprocessed_data, output_preprocessor, seed):
     The data is then preprocessed for use in model training, and the preprocessed data
     is also stored in csv files.
     """
-    # Hardcoding columns to drop (based on TA advice)
-    bank_marketing_data = pd.read_csv(raw_data)
-    bank_marketing_data = bank_marketing_data.drop(["poutcome", "contact"], axis = 1)
+    # Read train and test data 
+    bank_marketing_train = pd.read_csv(raw_train_data)
+    bank_marketing_test = pd.read_csv(raw_test_data)
+
+    # Hardcoding uninsightful columns to drop (based on TA advice)
+    bank_marketing_train = bank_marketing_train.drop(["poutcome", "contact"], axis = 1)
+    bank_marketing_test = bank_marketing_test.drop(["poutcome", "contact"], axis = 1)
     # Drop NAs
-    bank_marketing_data = bank_marketing_data.dropna()
-    
+    bank_marketing_train = bank_marketing_train.dropna()
+    bank_marketing_test = bank_marketing_test.dropna()
+    # Splitting to X and y test and train
+    X_train = bank_marketing_train.drop(columns=subscribed)
+    y_train = bank_marketing_train[subscribed]
+    X_test = bank_marketing_test.drop(columns=subscribed)
+    y_test = bank_marketing_test[subscribed]
+
     np.random.seed(seed)
     set_config(transform_output="pandas")
 
-    # Creating split
-    bank_marketing_train, bank_marketing_test = train_test_split(
-    bank_marketing_data, train_size=0.10, stratify=bank_marketing_data["subscribed"]
-    )
-    X_train = bank_marketing_train.drop(columns=target)
-    y_train = bank_marketing_train[target]
-    X_test = bank_marketing_test.drop(columns=target)
-    y_test = bank_marketing_test[target]
-
-    # Separating features for column transformer
+    # Making column transformer
     numeric_features = ['age', 'balance', 'duration', 'campaign', 'pdays', 'previous']
     categorical_features = ['job', 'marital']
     ordinal_features = ['education']
     binary_features = ['default', 'housing', 'loan']
     drop_features = ['day_of_week', 'month']
-    target = "subscribed"
 
     numeric_transformer = StandardScaler()
     categorical_transformer = OneHotEncoder(handle_unknown="ignore", sparse_output=False)
     education_order = ['primary', 'secondary', 'tertiary']
     ordinal_transformer = OrdinalEncoder(categories=[education_order], dtype=int)
     binary_transformer = OneHotEncoder(drop = 'if_binary', dtype=int, handle_unknown = "ignore", sparse_output=False)
+
     preprocessor = make_column_transformer(
     (numeric_transformer, numeric_features),
     (ordinal_transformer, ordinal_features),
@@ -63,7 +64,7 @@ def main(raw_data, output_preprocessed_data, output_preprocessor, seed):
     ("drop", drop_features),  
     )
     
-    # Pickle preprocessor object
+    # Pickle unfit preprocessor object
     pickle.dump(preprocessor, open(os.path.join(output_preprocessor, "preprocessor.pickle"), "wb"))
 
     # Fit preprocessor
@@ -71,7 +72,7 @@ def main(raw_data, output_preprocessed_data, output_preprocessor, seed):
     preprocessed_X_train = preprocessor.transform(X_train)
     preprocessed_X_test = preprocessor.transform(X_test)
 
-    # write raw data "data/processed" directory
+    # Write raw data "data/processed" directory
     preprocessed_X_train.to_csv(os.path.join(output_preprocessed_data, "preprocessed_X_train.csv"))
     preprocessed_X_test.to_csv(os.path.join(output_preprocessed_data, "preprocessed_X_test.csv"))
 
