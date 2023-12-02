2fdf2ddd6d8e0b541f703bdbe08cd5966c488f35
chenhanx <chenhanx@student.ubc.ca>
2023-12-02 00:01:36 -0800

    finish knn script, format preprocessor script

---
+++ scripts/knn_model.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import click
+import pickle
+import numpy as np
+import pandas as pd
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from src.models.knn_model import KNNModel
+
+
+@click.command()
+@click.option('--preprocessor_path', type=str, help="PATH of the preprocessor object")
+@click.option('--train_data_path', type=str, help="PATH of train dataset to read")
+@click.option('--model_save_path', type=str, help="Path of the model file to save")
+@click.option('--table_save_path', type=str, help="Path of the table file to save")
+@click.option('--plot_save_path', type=str, help="Path of the plot file to save")
+def main(preprocessor_path, train_data_path, model_save_path, table_save_path, plot_save_path):
+    with open(preprocessor_path, 'rb') as file:
+        preprocessor = pickle.load(file)
+
+    train_df = pd.read_csv(train_data_path)
+    # Hardcoding uninsightful columns to drop (based on TA advice)
+    train_df = train_df.drop(["poutcome", "contact"], axis = 1)
+    # Drop NAs
+    train_df = train_df.dropna()
+    X_train = train_df.drop(columns=["subscribed"])
+    y_train = train_df["subscribed"]
+
+    model = KNNModel(np.arange(1, 10, 2))
+    model.set_preprocessor(preprocessor)
+    model.search_cv(X_train, y_train)
+    with open(model_save_path, 'wb') as file:
+        pickle.dump(model.model, file)
+    accuracies_grid = model.get_accuracy_grid()
+    accuracies_grid.to_csv(table_save_path)
+    plot = model.draw_search_plot()
+    plot.save(plot_save_path)
+
+
+if __name__ == "__main__":
+    main()
