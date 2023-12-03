aa01b8fa8a2215bebbe45968358d5235639c18dd
chenhanx <chenhanx@student.ubc.ca>
2023-12-02 16:07:50 -0800

    update eda.py

--- scripts/eda.py
+++ scripts/eda.py
@@ -2,39 +2,44 @@ import click
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import altair as alt
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from src.viz.count_missing_values import count_missing_values
 from src.viz.plot_value_counts import plot_value_counts
-import os
+
 
 @click.command()
-@click.option('--train_path', type=int, help="Train dataset path")
+@click.option('--train_path', type=str, help="Train dataset path")
+@click.option('--table_path', type=str, help="Path to save tables")
 @click.option('--plot_path', type=str, help="Path to save plots")
-
-def main(train_path, plot_path):
+def main(train_path, table_path, plot_path):
     """EDA of the dataset"""
     bank_marketing_train = pd.read_csv(train_path)
 
     print(bank_marketing_train.info())
     bank_marketing_summary = bank_marketing_train.describe(include = 'all')
-    print(bank_marketing_summary)
+    bank_marketing_summary.to_csv(os.path.join(table_path, "eda", "data_summary.csv"))
     missing_values = count_missing_values(bank_marketing_train)
-    print(missing_values)
+    missing_values.to_csv(os.path.join(table_path, "eda", "missing_values.csv"))
     print("Length of training data: ", len(bank_marketing_train))
 
     # distribution of numerical columns
     numeric_cols = bank_marketing_train.select_dtypes(include=['number']).columns.to_list()
-    for i in numeric_cols:
-        feature = i
-        plt.figure(figsize=(4, 3))
-        plot = bank_marketing_train.groupby("subscribed")[feature].plot.hist(bins=20, alpha = 0.5, legend = True, density = True, title = "Histogram of " + feature)
-        plt.xlabel(feature)
-        #plt.show()
-        plot.save(os.path.join(plot_path, "feature_"+feature+".png"))
+    plot = alt.Chart(bank_marketing_train).mark_bar(opacity=0.5).encode(
+        x=alt.X(alt.repeat()).bin(maxbins=30),
+        y=alt.Y("count()").stack(False).title("Count"),
+        color="subscribed"
+    ).repeat(
+        numeric_cols,
+        columns=2
+    )
+    plot.save(os.path.join(plot_path, "numeric_cols.png"))
 
     # target class imbalance
-    plot_class_imbalance = plot_value_counts(bank_marketing_train, 'subscribed')
-    plot.save(os.path.join(plot_class_imbalance, "class_imbalance.png"))
-
+    fig, ax, bar = plot_value_counts(bank_marketing_train, 'subscribed')
+    fig.savefig(os.path.join(plot_path, "class_imbalance.png"), format='png')
 
     # unique values in categorical columns
     cat_cols = ["job", "marital", "education"]
